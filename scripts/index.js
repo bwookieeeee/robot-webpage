d8988e50c7be56bc7971d85878ef07b8d2d21923
Bryan Morrison <skeeter_mcbee@live.com>
2018-12-16 23:37:52 -0800

    add index.js

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $('#robot_type_selector').on('change', function() {
+        var targetdiv = document.getElementById(this.value);
+        var divs = document.getElementsByClassName("robot_type");
+        
+        if (targetdiv.id === 'none') {
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].style.display = 'none';
+            }
+            return;
+        } else {
+            for (var i = 0; i < divs.length; i++) {
+                if (divs[i] != targetdiv) {
+                    divs[i].style.display = 'none';
+                } else {
+                    divs[i].style.display = 'block';
+                }
+            }
+        }
+    });
+
+    $('#camera_type_selector').on('change', function() {
+        var targetdiv = document.getElementById(this.value);
+        var divs = document.getElementsByClassName("camera_type");
+
+        if (targetdiv.id === 'none') {
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].style.display = 'none';
+            }
+            return;
+        } else {
+            for (var i = 0; i < divs.length; i++) {
+                if (divs[i] != targetdiv) {
+                    divs[i].style.display = 'none';
+                } else {
+                    divs[i].style.display = 'block';
+                }
+            }
+        }
+    });
+
+    $('#tts_type_selector').on('change', function() {
+        var targetdiv = document.getElementById(this.value);
+        var divs = document.getElementsByClassName("tts_type");
+
+        if (targetdiv.id === 'none') {
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].style.display = 'none';
+            }
+            return;
+        } else {
+            for (var i = 0; i < divs.length; i++) {
+                if (divs[i] != targetdiv) {
+                    divs[i].style.display = 'none';
+                } else {
+                    divs[i].style.display = 'block';
+                }
+            }
+        }
+    });
+});
